9a2f7cda365ae598a47f631eed1711d82becfcb8
Martin Suasnavar <msuasnavar21@gmail.com>
2023-10-11 12:41:28 -0300

    Updated MyImage and MyVideo names to Image and Video respectly for consistency along other components

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import WhiteSpace from "./WhiteSpace/WhiteSpace";
 import LineSeparator from "./LineSeparator/LineSeparator";
-import MyImage from "./MyImage/MyImage";
-import MyVideo from "./MyVideo/MyVideo";
+import MyImage from "./Image/Image";
+import MyVideo from "./Video/Video";
 import Grid from "./Grid/Grid";
 
 export { WhiteSpace, LineSeparator, MyImage, MyVideo, Grid };
